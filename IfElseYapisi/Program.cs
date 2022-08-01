6677fa5b73b9b61ed937c8ae7af0d4b31c14b26b
pc <pc@PC-BILGISAYAR>
2022-08-01 20:06:34 +0300

    01.08.2022 If Else Yapısı

---
+++ IfElseYapisi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IfElseYapisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if else çalışma
+
+
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın!");
+            if (time <= 18)
+                Console.WriteLine("İyi Günler!");
+            else
+                Console.WriteLine("İyi Akşamlar!");
+
+            string zaman = (time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "İyi Günler!" : "İyi Akşamlar!");
+
+            Console.WriteLine(zaman);
+
+            Console.ReadLine();
+        }
+    }
+}
